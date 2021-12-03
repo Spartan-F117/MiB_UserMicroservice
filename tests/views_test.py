7e91e42491f4d4e5024a987e268a371cc6fe2d4a
leonardotalerico <leonardo.talerico@hotmail.it>
2021-12-03 04:11:23 -0500

    fix user test, coverage 82%

--- tests/views_test.py
+++ tests/views_test.py
@@ -1,4 +1,9 @@
 import unittest
+import json
+from flask import jsonify, request
+from flask.wrappers import Response
+
+
 
 
 class ViewTest(unittest.TestCase):
@@ -7,6 +12,7 @@ class ViewTest(unittest.TestCase):
     all classes that tests resources
     """
     client = None
+    
 
     @classmethod
     def setUpClass(self):
@@ -14,32 +20,84 @@ class ViewTest(unittest.TestCase):
         app = create_app()
         self.client = app.test_client()
 
-    def test_create_user(self):
+    def test_a_create_user(self):
     
         print('trying creating user....')
 
         payload = dict(email='bob@example.com', 
-                    password='pass123', date_of_birth='01/01/1999', 
+                    password='pass123', date_of_birth='1999-01-01', 
                     firstname='Bob', lastname='Johnson', 
                     nickname='Bobby', location='Pisa')
         
         response = self.client.post("/create_user", json=payload)
+
+        assert response.status_code == 201
+
+    def test_a_create_user_same_email(self):
+    
+        print('trying creating user....')
+
+        payload = dict(email='bob@example.com', 
+                    password='pass123', date_of_birth='1999-01-01', 
+                    firstname='Bob', lastname='Johnson', 
+                    nickname='Bobby', location='Pisa')
         
+        response = self.client.post("/create_user", json=payload)
+
+        assert response.status_code == 202
+    
+    def test_a_create_user_same_nick(self):
+    
+        print('trying creating user....')
+
+        payload = dict(email='bobbob@example.com', 
+                    password='pass123', date_of_birth='1999-01-01', 
+                    firstname='Bob', lastname='Johnson', 
+                    nickname='Bobby', location='Pisa')
+        
+        response = self.client.post("/create_user", json=payload)
+
         assert response.status_code == 202
 
-    def test_login(self):
+    def test_a_create_user_invalid_date(self):
+    
+        print('trying creating user....')
+
+        payload = dict(email='bob4@example.com', 
+                    password='pass123', date_of_birth='01-01_1999', 
+                    firstname='Bob', lastname='Johnson', 
+                    nickname='Bobby4', location='Pisa')
+        
+        response = self.client.post("/create_user", json=payload)
+
+        assert response.status_code == 202
+    
+    def test_a_create_user_born_in_future(self):
+    
+        print('trying creating user....')
+
+        payload = dict(email='bob3@example.com', 
+                    password='pass123', date_of_birth='2300-01-01', 
+                    firstname='Bob', lastname='Johnson', 
+                    nickname='Bobby3', location='Pisa')
+        
+        response = self.client.post("/create_user", json=payload)
+
+        assert response.status_code == 202
+
+    def test_ab_login(self):
 
         print('trying login....')
 
         payload = dict(email='bob@example.com', password='pass123')
         response = self.client.post("/authenticate", json=payload)
-        assert response.status_code == 201
+        assert response.status_code == 200
 
-    def test_logout(self):
+    def test_z_logout(self):
 
         print('trying logout....')
 
-        response = self.client.post("/logout/bob@example.com")
+        response = self.client.get("/logout/bob@example.com")
         assert response.status_code == 200
 
     def test_show_user(self):
@@ -67,7 +125,7 @@ class ViewTest(unittest.TestCase):
 
         print('trying blacklist info....')
 
-        response = self.client.post("/blacklist/2/5")
+        response = self.client.get("/blacklist_info/2/5")
         assert response.status_code == 202
 
     def test_delete_blacklist(self):
@@ -100,7 +158,7 @@ class ViewTest(unittest.TestCase):
 
         print('trying profile filter....')
 
-        response = self.client.post("/profile_filter/2")
+        response = self.client.get("/profile_filter/2")
         assert response.status_code == 201
 
     def test_change_filter(self):
@@ -115,17 +173,27 @@ class ViewTest(unittest.TestCase):
 
         print('trying change info....')
 
-        payload = dict(user_id='2', firstname='first_name', lastname='last_name',
-                    date_of_birth='02/02/1998', new_password='pass0000',
+        payload = dict(user_id='1', firstname='first_name', lastname='last_name',
+                    birthday='1998-02-02', new_password='pass0000',
                     old_password='pass123', location='Livorno')
+        response = self.client.post("/change_info", json=payload)
+        assert response.status_code == 201
+    
+    def test_change_info_wrong_password(self):
+
+        print('trying change info....')
+
+        payload = dict(user_id='1', firstname='first_name', lastname='last_name',
+                    birthday='1998-02-02', new_password='pass0000',
+                    old_password='pass_wrong', location='Livorno')
         response = self.client.post("/change_info", json=payload)
         assert response.status_code == 202
 
-    def test_delete_user(self):
+    def test_zz_delete_user(self):
 
         print('trying delete user....')
 
-        payload = dict(user_id='2')
+        payload = dict(user_id='1')
         response = self.client.post("/delete_user", json=payload)
         json_response = response.json
 
@@ -138,5 +206,5 @@ class ViewTest(unittest.TestCase):
 
         print('trying decrease lottery points....')
 
-        response = self.client.post("/decrease_lottery_points/5")
+        response = self.client.get("/decrease_lottery_points/1")
         assert response.status_code == 200
